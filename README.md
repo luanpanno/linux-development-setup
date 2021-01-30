f23cc0653dc37f00f8895c091b84d291dc79e8dd
Luan Panno <luanpanno@gmail.com>
2021-01-30 18:55:36 -0300

    feat: update to english

--- README.md
+++ README.md
@@ -1,36 +1,60 @@
-# Setup de desenvolvimento no Linux de Luan Panno
+# Linux Development Setup by Luan Panno
 
-## O que é instalado
-1. Google Chrome
-2. Curl
-3. Git
-4. Node.js
-5. VSCode
-6. Extensões do VSCode
-7. Spotify
-8. Insomnia
-9. Flameshot
-10. Peek
-11. Create-React-App
-12. Yarn
-13. Docker
-14. Docker Compose
-15. ZSH
-16. Oh my Zsh
+Inspired by Erick Wendel's [ew-ubuntu-setup repository](https://github.com/ErickWendel/ew-ubuntu-setup) :)
 
-## Como executar
-Abra o terminal e baixe o arquivo compactado na pasta desejada executando a seguinte linha
+- [What's gonna be installed](#whats-gonna-be-installed)
+- [VSCode Extensions](#vscode-extensions)
+- [How to run the setup](#how-to-run-the-setup)
+
+## What's gonna be installed
+
+1. [Google Chrome](https://www.google.com/chrome/)
+2. [Curl](https://curl.se/)
+3. [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+4. [Node.js](https://github.com/nodesource/distributions/blob/master/README.md#debinstall)
+5. [VSCode](https://code.visualstudio.com/)
+6. [My most used VSCode Extensions](#vscode-extensions)
+7. [Spotify](https://www.spotify.com/br/download/linux/)
+8. [Insomnia](https://support.insomnia.rest/article/23-installation)
+9. [Flameshot](https://github.com/flameshot-org/flameshot#installation)
+10. [Peek](https://github.com/phw/peek#ubuntu)
+11. [Remmina](https://remmina.org/how-to-install-remmina/)
+12. [Yarn](https://classic.yarnpkg.com/en/docs/install/#debian-stable)
+13. [Docker](https://docs.docker.com/engine/install/ubuntu/)
+14. [Docker Compose](https://docs.docker.com/compose/install/)
+15. [ZSH](https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH)
+16. [Oh my Zsh](https://ohmyz.sh/#install)
+
+## VSCode Extensions
+
+- [Color Highlight](https://marketplace.visualstudio.com/items?itemName=naumovs.color-highlight)
+- [Docker](https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-docker)
+- [Editor Config for VS Code](https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig)
+- [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+- [GitLens — Git supercharged](https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens)
+- [Jest Snippets](https://marketplace.visualstudio.com/items?itemName=andys8.jest-snippets)
+- [Markdown Preview Enhanced](https://marketplace.visualstudio.com/items?itemName=shd101wyy.markdown-preview-enhanced)
+- [Material Icon Theme](https://marketplace.visualstudio.com/items?itemName=PKief.material-icon-theme)
+- [One Monokai Theme](https://marketplace.visualstudio.com/items?itemName=azemoh.one-monokai)
+- [Prettier - Code formatter](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode)
+- [React-Native/React/Redux snippets for es6/es7](https://marketplace.visualstudio.com/items?itemName=EQuimper.react-native-react-redux)
+- [Reload](https://marketplace.visualstudio.com/items?itemName=natqe.reload)
+- [Remote - Containers](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers)
+- [TSLint](https://marketplace.visualstudio.com/items?itemName=ms-vscode.vscode-typescript-tslint-plugin)
+- [vscode-styled-components](https://marketplace.visualstudio.com/items?itemName=jpoissonnier.vscode-styled-components)
+
+## How to run the setup
+
+Open the terminal, go to the directory you want to download the setup, and then run this line
 
     wget https://github.com/luanpanno/linux-setup/archive/master.zip -O linux-setup.zip
 
-Depois de baixado, extraia, vá para a pasta extraída no terminal e digite
+It will come zipped, so you must unzip the file, then go to its directory and run
 
     chmod +x setup.sh
 
-Em seguida, rode o setup com
+Then, execute the setup running the following line
 
     ./setup.sh
-    
-Também vai acompanhado meus arquivos de settings e keybinds do VSCode!
 
-Inspirado no repositório "ew-ubuntu-setup" do Erick Wendel ([link do repositório](https://github.com/ErickWendel/ew-ubuntu-setup)) :)
+It also includes my settings and keyboard shortcuts from VSCode and my Git Config file!
--- setup.sh
+++ setup.sh
@@ -1,38 +1,44 @@
 echo '-------------------------------------------------'
-echo 'Setup para desenvolvimento no Linux de Luan Panno'
+echo 'Linux Development Setup - Luan Panno'
 echo '-------------------------------------------------'
 
-echo 'executando um update'
+echo 'running update'
 sudo apt-get update
 
 echo '-------------------------------------------------'
-echo 'instalando Google Chrome' 
+echo 'installing Google Chrome' 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 echo '-------------------------------------------------'
-echo 'instalando curl' 
+echo 'installing curl' 
 sudo apt-get install curl -y
 
 echo '-------------------------------------------------'
-echo 'instalando git' 
-sudo apt-get install git -y
+echo 'installing git' 
+sudo apt-get install git-all -y
 
 echo '-------------------------------------------------'
-git config --global user.name "Luan Panno"
-echo 'Luan Panno adicionado como user.name no git'
+echo "What name do you want to use in GIT user.name?"
+echo "For example, mine will be \"Luan Panno\""
+read git_config_user_name
+git config --global user.name "$git_config_user_name"
+clear 
 
 echo '-------------------------------------------------'
-git config --global user.email "luanpanno@gmail.com"
-echo 'luanpanno@gmail.com adicionado como user.email no git'
+echo "What email do you want to use in GIT user.email?"
+echo "For example, mine will be \"luanpanno@gmail.com\""
+read git_config_user_email
+git config --global user.email $git_config_user_email
+clear
 
 echo '-------------------------------------------------'
-echo 'instalando Node.js'
+echo 'installing Node.js LTS'
 curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 echo '-------------------------------------------------'
-echo 'instalando VSCode'
+echo 'installing VSCode'
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -41,29 +47,31 @@ sudo apt-get update
 sudo apt-get install code -y # or code-insiders
 
 echo '-------------------------------------------------'
-echo 'instalando extensões no VSCode'
+echo 'installing VSCode Extensions'
 code --install-extension naumovs.color-highlight
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension editorconfig.editorconfig
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
+code --install-extension andys8.jest-snippets
 code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension PKief.material-icon-theme
 code --install-extension azemoh.one-monokai
 code --install-extension esbenp.prettier-vscode
 code --install-extension EQuimper.react-native-react-redux
+code --install-extension natqe.reload
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension jpoissonnier.vscode-styled-components
 
 echo '-------------------------------------------------'
-echo 'instalando spotify'
+echo 'installing spotify'
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client -y
 
 echo '-------------------------------------------------'
-echo 'instalando insomnia'
+echo 'installing insomnia'
 echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
     | sudo tee -a /etc/apt/sources.list.d/insomnia.list
 
@@ -73,21 +81,27 @@ wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
 sudo apt-get update && sudo apt-get install insomnia -y
 
 echo '-------------------------------------------------'
-echo 'instalando flameshot'
+echo 'installing flameshot'
 sudo apt-get install flameshot -y
 
 echo '-------------------------------------------------'
-echo 'instalando peek'
+echo 'installing peek'
 sudo apt-get install peek -y
 
 echo '-------------------------------------------------'
-echo 'instalando yarn'
+echo 'installing remmina'
+sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+sudo apt update
+sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret -y
+
+echo '-------------------------------------------------'
+echo 'installing yarn'
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
 
 echo '-------------------------------------------------'
-echo 'instalando docker' 
+echo 'installing docker' 
 sudo apt-get remove docker docker-engine docker.io
 sudo apt install docker.io -y
 sudo systemctl start docker
@@ -98,17 +112,17 @@ sudo chmod 777 /var/run/docker.sock
 docker run hello-world
 
 echo '-------------------------------------------------'
-echo 'instalando docker-compose' 
+echo 'installing docker-compose' 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
 
 echo '-------------------------------------------------'
-echo 'instalando zsh'
+echo 'installing zsh'
 sudo apt-get install zsh -y
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /bin/zsh
 
 echo '-------------------------------------------------'
-echo '\n Setup finalizado! :) \n'
+echo '\n Setup done! :) \n'
